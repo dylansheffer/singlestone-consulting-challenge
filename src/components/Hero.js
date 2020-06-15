0bdd12f572e1ba4e83dea5abf11e257c8c3e61ef
Dylan Sheffer <dylan@dylansheffer.com>
2020-06-15 16:45:50 -0400

    feat: hero content and basic styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,3 +1,18 @@
 import React from 'react';
+import { ContentContainer } from './ContentContainer';
 
-export const Hero = () => (<nav className="hero">Hero</nav>)
+export const Hero = () => (
+  <section className="hero">
+    <ContentContainer>
+      <h1 className="visually-hidden">Endless</h1>
+      <h2 className="hero__title">
+        <div className="hero__title--small">New Games & Accessories</div>
+        <div>Monthly packages.</div>
+        <div>Excitement delivered daily.</div>
+      </h2>
+      <p className="hero__content">
+        What's the best way to shop for the latest video games and peripherals? How about never shopping at all? You'll get new stuff on your doorstep ― every month.
+      </p>
+    </ContentContainer>
+  </section>
+);
