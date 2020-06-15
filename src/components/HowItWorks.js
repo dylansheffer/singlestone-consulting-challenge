bbae8b77f2f96ef947514c54df1b5b099adf4f78
Dylan Sheffer <dylan@dylansheffer.com>
2020-06-14 21:34:46 -0400

    feat: initial commit

---
+++ src/components/HowItWorks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const HowItWorks = () => (
+  <section className="how-it-works">
+    <ol>
+      <li>How</li>
+      <li>It</li>
+      <li>Works</li>
+      <li>!</li>
+    </ol>
+  </section>
+);
